aaa2faa53c7e6548375ce5cce270cc029f23d467
uotani-kei <2004240003uk@edu.oca.ac.jp>
2022-06-18 14:57:02 +0900

    appppp

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
 def inex():
@@ -7,3 +7,12 @@ def inex():
 @app.route('/another')
 def fla():
     return 'Another Response'
+
+@app.route('/test_request')
+def test_request():
+    return f'test_request:{request.args.get("dummy")}'
+
+@app.route('/x_request/<user>')
+def x_request(user):
+    return f'x_request:{user}'
+
