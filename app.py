971b7da5229aab237e4cd5a77710c984c7b6d6c5
uotani-kei <2004240003uk@edu.oca.a.jp>
2022-07-02 14:53:18 +0900

    try

--- app.py
+++ app.py
@@ -38,3 +38,11 @@ def try_rest():
     print(name)
     response_json = {"response_json": request_json}
     return jsonify(response_json)
+
+@app.route('/try_html')
+def try_html():
+    return render_template('try_html.html')
+
+@app.route('/show_data', methods=["GET", "POST"])
+def show_data():
+    return request.form
